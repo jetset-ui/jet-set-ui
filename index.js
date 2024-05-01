3cf7503ccf5adfba9c0c483d16e5edd7b6bb2181
asifazad799 <asifazad799@gmail.com>
2024-05-01 23:25:20 +0400

    fix - removed unused codes

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ async function copyComponent(componentName) {
     }
 
     console.log(
-      `Component '${componentName}' copied successfully to '${targetDirectory}'.`
+      `\u001b[32m Component '${componentName}' copied successfully to '${targetDirectory}'.`
     );
   } catch (error) {
     console.error("Error:", error);
@@ -59,8 +59,7 @@ if (!componentName) {
 // Check if npm install is attempted
 if (
   process.env.npm_config_argv &&
-  (process.env.npm_config_argv.includes("install") ||
-    process.env.npm_config_argv.includes("i"))
+  process.env.npm_config_argv.includes("install")
 ) {
   console.error(
     "This package cannot be installed via npm. Please use npx to execute it."
@@ -70,55 +69,3 @@ if (
 
 // Call the function
 copyComponent(componentName);
-
-// const fs = require("fs");
-// const path = require("path");
-
-// function createPackage(componentName) {
-//   // Validate component name
-//   if (!componentName) {
-//     console.error("Component name is required.");
-//     return;
-//   }
-//   // Validate component name for invalid characters (optional)
-//   if (!isValidComponentName(componentName)) {
-//     console.error("Invalid component name.");
-//     return;
-//   }
-
-//   // Create directory
-//   const directoryPath = path.join(process.cwd(), componentName);
-//   try {
-//     fs.mkdirSync(directoryPath);
-//   } catch (err) {
-//     console.error("Error creating directory:", err);
-//     return;
-//   }
-
-//   // Copy file
-//   const templateFilePath = path.join(__dirname, "template.txt"); // Assuming template.txt is your template file
-//   try {
-//     fs.copyFileSync(
-//       templateFilePath,
-//       path.join(directoryPath, "copiedFile.txt")
-//     );
-//   } catch (err) {
-//     console.error("Error copying file:", err);
-//     return;
-//   }
-
-//   console.log(`Package '${componentName}' created successfully!`);
-// }
-
-// // Validate component name for invalid characters (optional)
-// function isValidComponentName(componentName) {
-//   return /^[a-zA-Z0-9_-]+$/.test(componentName);
-// }
-
-// module.exports = createPackage;
-
-// // Run function if executed directly
-// if (require.main === module) {
-//   const componentName = process.argv[2];
-//   createPackage(componentName);
-// }
